9a2e8af993430eb1ac82864dc33da04b5223414d
Matt Petrowsky <matt@isoproductions.com>
2012-04-04 19:20:18 -0700

    Initial commit

---
+++ config.rb
@@ -0,0 +1,14 @@
+# This is the configuration file for compass
+# http://compass-style.org/
+
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'theme'
+sass_dir = 'theme'
+images_dir = 'theme'
+javascripts_dir = 'theme'
+relative_assets = true
+line_comments = true
+
+output_style = :expanded
+#output_style = :compressed
